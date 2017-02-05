986afbe4b40200a627429324007e229046f13a53
Maksym Solomkin <solomkinmv@gmail.com>
2017-02-05 21:30:31 +0200

    Add content-type

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,12 +1,16 @@
 import {Injectable} from "@angular/core";
-import {Http, Response} from "@angular/http";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
 
 @Injectable()
 export class AuthenticationService {
   constructor(private http: Http) { }
 
   login(username: string, password: string) {
-    return this.http.post('/api/auth/login', JSON.stringify({ username: username, password: password }))
+    const body = JSON.stringify({username: username, password: password});
+    const headers = new Headers({'Content-Type': 'application/json'});
+    const options = new RequestOptions({'headers': headers});
+
+    return this.http.post('/api/auth/login', body, options)
       .map((response: Response) => {
         let user = response.json();
         if (user && user.token) {
