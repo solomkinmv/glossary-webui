f09437ce8be56c590b922af42564ba7727a6c9b3
Maksym Solomkin <solomkinmv@gmail.com>
2017-03-07 22:46:07 +0200

    Basic WritePractice component

--- src/app/dictionary/dictionary-routing.module.ts
+++ src/app/dictionary/dictionary-routing.module.ts
@@ -2,10 +2,12 @@ import {Routes, RouterModule} from "@angular/router";
 import {DictionaryComponent} from "./dictionary.component";
 import {NgModule} from "@angular/core";
 import {WordSetComponent} from "./word-set.component";
+import {WritePracticeComponent} from "./write-practice.component";
 
 const routes: Routes = [
   {path: '', component: DictionaryComponent},
   {path: 'set/:id', component: WordSetComponent},
+  {path: 'practice/:id', component: WritePracticeComponent},
 ];
 
 @NgModule({
