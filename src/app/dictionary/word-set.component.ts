8b05a07e1c7d87a431599cbc656aec7cd023c43f
Yevheniy Miropolets <yevheniy.miropolets@gmail.com>
2017-03-10 00:02:44 +0200

    Initial commit for add word feature

--- src/app/dictionary/word-set.component.ts
+++ src/app/dictionary/word-set.component.ts
@@ -2,15 +2,21 @@ import {Component, OnInit} from "@angular/core";
 import {WordSet} from "./word-set";
 import {WordSetService} from "../_services/word-set.service";
 import {ActivatedRoute, Params} from "@angular/router";
+import {WordService} from '../_services/word.service';
+import {Word} from './word';
 
 @Component({
   templateUrl: './word-set.component.html'
 })
 export class WordSetComponent implements OnInit {
-  wordSet: WordSet;
+  private wordSet: WordSet;
+  private newWordText: string;
+  private newWordTranslation: string;
+
 
   constructor(private wordSetService: WordSetService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private wordService: WordService) {
   }
 
   ngOnInit(): void {
@@ -22,4 +28,19 @@ export class WordSetComponent implements OnInit {
         console.log(this.wordSet);
       });
   }
+
+  addWord(): void {
+    const word = new Word(null, this.newWordText, this.newWordTranslation); 
+    this.wordService.add(word)
+      .then((newWordId: number) => {
+        word.id = newWordId;
+        console.log({word});  
+        return this.wordSetService.addWord(this.wordSet, word);
+      })
+      .then((wordSet: WordSet) => {
+        this.wordSet = wordSet;
+      })
+      .catch((err) => console.log(err));
+  }
+
 }
--- src/app/dictionary/word.ts
+++ src/app/dictionary/word.ts
@@ -1,7 +1,8 @@
 export class Word {
-  id: number;
-  text: string;
-  translation: string;
+	constructor(public id: number,
+	 						public text: string,
+	 						public translation: string) {
+	}
 
   toString(): string {
     return `Word(id=${this.id},text=${this.text},translation=${this.translation})`;
