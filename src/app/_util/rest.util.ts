62e98bd691bee921827572ca5f6f80b351778cdc
Maksym Solomkin <solomkinmv@gmail.com>
2017-04-10 21:46:17 +0300

    Extend utils

--- src/app/_util/rest.util.ts
+++ src/app/_util/rest.util.ts
@@ -1,8 +1,28 @@
 import {Response} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
 export class RestUtils {
 
   public static getIdFromLocationHeader(response: Response): number {
-    return +response.headers.get('Location').split('/').pop();
+    return this.getIdFromLocation(response.headers.get('Location'));
+  }
+
+  public static getIdFromLocation(location: string): number {
+    return +location.split('/').pop();
+  }
+
+  public static processGenericResponse(response: Observable<Response>): Observable<string> {
+    return response
+      .do((response: Response) => console.log(response))
+      .map((response: Response) => response.statusText)
+      .catch(RestUtils.serverError);
+  }
+
+  public static serverError(err: any): Observable<any> {
+    console.log('Server error:', err);
+    if (err instanceof Response) {
+      return Observable.throw(err.json()[0].message || 'Server error');
+    }
+    return Observable.throw(err || 'Server error');
   }
 }
