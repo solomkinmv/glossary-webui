14c888fff3b91f714e0667eecef18ddaa2387b0c
Maksym Solomkin <solomkinmv@gmail.com>
2017-04-17 01:12:55 +0300

    Add token check to app component

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,11 +1,17 @@
 import {Injectable} from "@angular/core";
 import {Headers, Http, RequestOptions, Response} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private http: Http) { }
+  constructor(private http: Http,
+              private router: Router) {
+  }
 
-  public login(username: string, password: string) {
+  private tokenKey = 'tokenHolder';
+
+  public login(username: string, password: string): Observable<Response> {
     const body = JSON.stringify({username: username, password: password});
     const headers = new Headers({'Content-Type': 'application/json'});
     const options = new RequestOptions({'headers': headers});
@@ -14,13 +20,22 @@ export class AuthenticationService {
       .map((response: Response) => {
         const tokenHolder = response.json();
         if (tokenHolder && tokenHolder.token) {
-          localStorage.setItem('tokenHolder', JSON.stringify(tokenHolder));
+          localStorage.setItem(this.tokenKey, JSON.stringify(tokenHolder));
         }
+
+        return response;
       })
   }
 
-  public logout() {
+  public logout(): void {
     // remove user from local storage to log user out
     localStorage.removeItem('tokenHolder');
+    this.router.navigate(['/']);
+  }
+
+  public containsToken(): boolean {
+    console.log('Checking token in local storage');
+    let item = localStorage.getItem('tokenHolder');
+    return item != null;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {AuthenticationService} from "./_services/authentication.service";
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html'
 })
+export class AppComponent implements OnInit {
+  private loggedIn: boolean = false;
 
-export class AppComponent { }
+  constructor(private authService: AuthenticationService) {
+  }
+
+  ngOnInit(): void {
+    console.log('AppComponent init');
+    this.loggedIn = this.authService.containsToken();
+  }
+
+  private logout(): void {
+    console.log('Logging out');
+    this.authService.logout();
+  }
+}
